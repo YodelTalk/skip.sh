42b04fc37608edc14d419f3d8d7a50ab34a80021
Mario Uher <uher.mario@gmail.com>
2020-11-13 11:36:35 +0100

    Invert logic

--- skip.sh
+++ skip.sh
@@ -3,7 +3,7 @@
 folders=(`git diff-tree --no-commit-id --name-only master..HEAD`)
 
 for folder in "${folders[@]}"; do
-    if [[ "${folder}" =~ $FILE_PATTERN ]]; then exit 0; fi
+    if [[ "${folder}" =~ $FILE_PATTERN ]]; then exit 1; fi
 done
 
-exit 1
+exit 0
